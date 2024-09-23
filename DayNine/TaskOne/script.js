04a319473ba7bb7ba6aa53cb6e731ee6f0c0bb61
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-23 16:00:56 +0000

    Task One of Day 9 is Done

---
+++ DayNine/TaskOne/script.js
@@ -0,0 +1,7 @@
+function changeText() {
+    // Select the element by its ID
+    var element = document.getElementById("myElement");
+
+    // Change its text content
+    element.textContent = "Task One is Done i.e Text is Changed!";
+}
