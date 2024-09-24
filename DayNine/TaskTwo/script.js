57c65316d2f6aa7baa306bbe98a8e9651cd97c9a
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-24 12:37:08 +0000

    Task Two of Day 9

---
+++ DayNine/TaskTwo/script.js
@@ -0,0 +1,6 @@
+function changeColor() {
+    // Select the element by its class name
+    let element = document.querySelector('.color-change');
+    // Change the background color
+    element.style.backgroundColor = 'lightblue';
+}
