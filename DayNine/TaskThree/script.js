35bcc8817296dbef928097fe55012662977bfd70
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-27 03:39:11 +0000

    Task Three of Day 9

---
+++ DayNine/TaskThree/script.js
@@ -0,0 +1,9 @@
+// Create a new div element
+const newDiv = document.createElement('div');
+
+// Add text content to the div
+newDiv.textContent = 'This is a new div element added to the body.';
+
+// Append the div to the body
+document.body.appendChild(newDiv);
+
