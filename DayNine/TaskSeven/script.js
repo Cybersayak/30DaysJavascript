6d682d5efe5ba11a8beac6a8bc478e864b3a1605
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-10-01 13:57:21 +0000

    Task Seven of Day 9

---
+++ DayNine/TaskSeven/script.js
@@ -0,0 +1,9 @@
+// Select the image element by its ID
+const imageElement = document.getElementById('myImage');
+
+// Change the 'src' attribute of the image
+imageElement.setAttribute('src', 'image2.jpg');
+
+// Optionally, you can also change other attributes
+imageElement.setAttribute('alt', 'Updated Image');
+imageElement.setAttribute('width', '400'); // Change width attribute
