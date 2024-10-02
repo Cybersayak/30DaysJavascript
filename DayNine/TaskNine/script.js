6a45e1dc2dcdc0e7d170d7cee9b4ac51b7da39f0
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-10-02 17:59:19 +0000

    Task Nine of Day 9

---
+++ DayNine/TaskNine/script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    var paragraph = document.getElementById('myParagraph');
+    paragraph.textContent = 'The paragraph text has been changed!';
+});
